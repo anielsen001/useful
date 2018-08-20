60a314b0d4b1b56d6ee6226db6276801e0d94a80
Aaron Nielsen <apn@apnielsen.com>
2018-08-20 06:28:39 -0400

    updated start-vnc.sh

--- start-vnc.sh
+++ start-vnc.sh
@@ -5,3 +5,9 @@
 # and port
 #
 vncserver -geometry 1298x950 :9
+
+# the vnc server config can be found in $HOME/.vnc
+# for CentOS 6 (at least) the xstatup file actually starts the
+# X session. I had to comment out lines at the end to get
+# it to start an gnome-session
+
